96cb6695d528cd59df95e121c144cefcbe1bac33
NowaDeCocow <chellenoah@gmail.com>
2024-06-11 15:57:34 +0200

    Noah

---
+++ js/login.js
@@ -0,0 +1,11 @@
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulaire d\'inscription soumis !');
+    // Ajoutez ici le code pour gérer la soumission du formulaire d'inscription
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulaire de connexion soumis !');
+    // Ajoutez ici le code pour gérer la soumission du formulaire de connexion
+});
